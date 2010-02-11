3c37afc28692c518789698f4d415e1631654aa6e
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-02-11 15:08:51 +0100

    PTraversable type class.

--- src/Generics/Family.hs
+++ src/Generics/Family.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses, RankNTypes, TypeOperators, TypeFamilies #-}
 module Generics.Family where
 
+import Control.Applicative
 import Generics.HigherOrder
 
 class PFunctor phi h where
@@ -10,4 +11,7 @@ type PPara phi f g = forall ix. phi ix -> f (HFix f :*: g) ix -> g ix
 
 ppara :: PFunctor phi f => PPara phi f g -> phi ix -> HFix f ix -> g ix
 ppara f phi (HIn u) = f phi (pfmap (\p x -> x :*: ppara f p x) phi u)
+
+class PFunctor phi h => PTraversable phi h where
+  ptraverse :: Applicative f => (forall ix. phi ix -> a ix -> f (b ix)) -> (forall ix. phi ix -> h a ix -> f (h b ix))
 
