cc893999a641743b130d0bb5615923de1c46f8a4
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-02-11 15:10:48 +0100

    Higher order annotation.

--- src/Annotation/HAnnotation.hs
+++ src/Annotation/HAnnotation.hs
@@ -1,38 +1,38 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, RankNTypes #-}
 module Annotation.HAnnotation where
 
 import Control.Applicative
 import Control.Arrow
 import Control.Category
-import Generics.Fixpoint
+import Generics.HigherOrder
 import Prelude hiding ((.), id)
 
--- todo: put forall ix. in these synonyms?
-type Produce a h ix m = Kleisli m (h (HFixA a h) ix)  (  (HFixA a h  ix))
-type Query   a h ix m = Kleisli m    (HFixA a h  ix)  (h (HFixA a h) ix)
-type Modify  a h ix m = Kleisli m (h (HFixA a h) ix)  (h (HFixA a h) ix)
-                     -> Kleisli m (  (HFixA a h  ix)) (  (HFixA a h  ix))
+type Produce a h m = forall ix. Kleisli m (h (HFixA a h) ix)  (  (HFixA a h  ix))
+type Query   a h m = forall ix. Kleisli m    (HFixA a h  ix)  (h (HFixA a h) ix)
+type Modify  a h m = forall ix. Kleisli m (h (HFixA a h) ix)  (h (HFixA a h) ix)
+                             -> Kleisli m (  (HFixA a h  ix)) (  (HFixA a h  ix))
 
-class (Applicative m, Monad m) => HAnnQ a h ix m where
-  query :: Query a h ix m
+class (Applicative m, Monad m) => HAnnQ a h m where
+  query :: Query a h m
 
-class (Applicative m, Monad m) => HAnnP a h ix m where
-  produce :: Produce a h ix m
+class (Applicative m, Monad m) => HAnnP a h m where
+  produce :: Produce a h m
 
-class (HAnnQ a h ix m, HAnnP a h ix m) => HAnnM a h ix m where
-  modify :: Modify a h ix m
+class (HAnnQ a h m, HAnnP a h m) => HAnnM a h m where
+  modify :: Modify a h m
   modify f = produce . f . query
 
-runQuery :: HAnnQ a h ix m => HFixA a h ix -> m (h (HFixA a h) ix)
+runQuery :: HAnnQ a h m => HFixA a h ix -> m (h (HFixA a h) ix)
 runQuery = runKleisli query
 
-runProduce :: HAnnP a h ix m => h (HFixA a h) ix -> m (HFixA a h ix)
+runProduce :: HAnnP a h m => h (HFixA a h) ix -> m (HFixA a h ix)
 runProduce = runKleisli produce
 
-instance (Applicative m, Monad m) => HAnnQ HId h ix m where
-  query = Kleisli (return . unHId . hout)
+instance (Applicative m, Monad m) => HAnnQ HId h m where
+  query = Kleisli (return . unHId . houtA)
 
-instance (Applicative m, Monad m) => HAnnP HId h ix m where
-  produce = Kleisli (return . HIn . HId)
+instance (Applicative m, Monad m) => HAnnP HId h m where
+  produce = Kleisli (return . HInA . HId)
 
-instance (Applicative m, Monad m) => HAnnM HId h ix m
+instance (Applicative m, Monad m) => HAnnM HId h m
 
